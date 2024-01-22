38ad30d9050cdb08f05d5291fe7bf29270e66d8d
AlexandreGRN <tulkiidra@gmail.com>
2024-01-22 12:05:31 +0100

    handled a warning

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -2,9 +2,11 @@
 
 void topple(int grid[3][3])
 {
-    int grid_copy[3][3] = {grid[0][0], grid[0][1], grid[0][2],
-                           grid[1][0], grid[1][1], grid[1][2],
-                           grid[2][0], grid[2][1], grid[2][2]};
+    int grid_copy[3][3] = {
+        {grid[0][0], grid[0][1], grid[0][2]},
+        {grid[1][0], grid[1][1], grid[1][2]},
+        {grid[2][0], grid[2][1], grid[2][2]}
+    };
 
     for (int i = 0 ; i < 3 ; i++)
         for (int j = 0 ; j < 3 ; j++)
