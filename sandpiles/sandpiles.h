82ddc2cdb50b7feb2c023d476bb20e4f556254db
AlexandreGRN <tulkiidra@gmail.com>
2024-01-22 12:27:07 +0100

    idndef double inclusion

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,5 +1,10 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
 #include <stdio.h>
 void topple(int grid[3][3]);
 void print_grid_nl(int grid[3][3]);
 int isStable(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif
