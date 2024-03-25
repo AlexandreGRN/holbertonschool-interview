c079fae360548bcd211aff11d27816dd3d1fe25a
AlexandreGRN <tulkiidra@gmail.com>
2024-03-25 11:06:18 +0000

    Push lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+def checkKeys(boxes, keyList):
+    if len(keyList) == len(boxes):
+        return True
+
+def canUnlockAll(boxes):
+    keyList = [0]
+    newKeyList = [0]
+    check = True
+    while check:
+        newKeyList = keyList
+        check = False
+        for key in keyList:
+            for key in boxes[key]:
+                if key < len(boxes) and key not in keyList:
+                    newKeyList.append(key)
+                    check = True
+        if checkKeys(boxes, newKeyList):
+            return True
+        keyList = newKeyList
+    return False
