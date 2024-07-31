2a78f6dce058c05bfaf027a973f0ed48f4834a0f
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2024-07-31 08:26:58 +0000

    Feat: rain algo

---
+++ rain/0-rain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Water retain algo
+"""
+
+
+def rain(walls):
+    water_retain = 0
+    for height in range(max(walls) + 1):
+        water_retain_last_puddle = 0
+        in_puddle = False
+        for column in range(len(walls)):
+            if (walls[column] >= height and in_puddle):
+                in_puddle = False
+                water_retain += water_retain_last_puddle
+                water_retain_last_puddle = 0
+            if (in_puddle):
+                water_retain_last_puddle += 1
+            if (walls[column] >= height):
+                in_puddle = True
+    return water_retain
